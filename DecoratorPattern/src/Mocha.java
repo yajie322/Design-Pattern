869038200442d30b5744f906ecf083c8e471bbee
yajie322 <yajie322@gmail.com>
2020-04-22 20:23:24 -0400

    Implemented Decorator Pattern

---
+++ DecoratorPattern/src/Mocha.java
@@ -0,0 +1,18 @@
+public class Mocha extends CondimentDecorator {
+
+    private Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + 0.2;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+}
